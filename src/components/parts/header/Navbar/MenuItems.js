e814e5289ee149f52b7d0c0c7d736a047cc386ee
loreen11 <loreen92015@gmail.com>
2021-04-06 18:48:19 +0300

    initial commit

---
+++ src/components/parts/header/Navbar/MenuItems.js
@@ -0,0 +1,32 @@
+export const MenuItems = [
+  {
+    title: 'כניסה',
+    url: '#',
+    cName: 'Nav-links'
+  },
+  {
+    title: 'פרופיל',
+    url: '#',
+    cName: 'Nav-links'
+  },
+  {
+    title: 'הוראות',
+    url: '#',
+    cName: 'Nav-links'
+  },
+  {
+    title: 'חנות',
+    url: '#',
+    cName: 'Nav-links'
+  },
+  {
+    title: 'פורום',
+    url: '#',
+    cName: 'Nav-links'
+  },
+  {
+    title: 'צור קשר',
+    url: '#',
+    cName: 'Nav-links-mobile'
+  }
+]
